7d7b00fb11c49844e9e33e245c2d5083885ab0c8
OracleOG <emmanuelnwafor212@gmail.com>
2025-01-31 16:50:21 +0100

    run on vercel

--- stage_0_backend/app.py
+++ stage_0_backend/app.py
@@ -8,9 +8,9 @@ CORS(app)  # Enable CORS
 @app.route("/", methods=["GET"])
 def get_info():
     return jsonify({
-        "email": "your-email@example.com",
+        "email": "emmanuelnwafor212@gmail.com",
         "current_datetime": datetime.utcnow().isoformat() + "Z",
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/OracleOG/HNG12/"
     })
 
 if __name__ == "__main__":
